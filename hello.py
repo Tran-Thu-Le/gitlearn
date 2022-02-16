8e7625d634e0e66819bde49596628e16ec9408e2
Tran-Thu-Le <longtranthienlocationtheory@gmail.com>
2022-02-16 11:20:00 +0100

    add one more print()

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ print("hello world from hello.py")
 
 for i in range(10):
 	# print(i**2)
-	print(f"Iteration {i}: value={i*2}")
+	print(f"Iteration {i}: square={i**2}")
+	print(f"Iteration {i}: double={2*i}\n")
 
 
