eac5574638beb278e8ebc52fb155262922c52fdf
Tran Thu Le <longtranthienlocationtheory@gmail.com>
2022-02-19 21:09:49 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 print("hello world from hello.py")
 
-for i in range(10):
-	# print(i**2)
-	print(f"Iteration {i}: square={i**2}")
-	print(f"Iteration {i}: double={2*i}\n")
-	print("xxx")
+# for i in range(10):
+# 	# print(i**2)
+# 	print(f"Iteration {i}: square={i**2}")
+# 	print(f"Iteration {i}: double={2*i}\n")
+# 	print("xxx")
+
+print("220219 a1 a new commit from remote (edit on github)")
 
 
