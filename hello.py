bfdd22cdda78785c091a4932e9b8506366579bf0
Tran-Thu-Le <longtranthienlocationtheory@gmail.com>
2022-02-19 22:10:42 +0100

    add small text inserted from local, this text may cause a conflict

--- hello.py
+++ hello.py
@@ -11,12 +11,14 @@ print("hello world from hello.py")
 
 # Conflicts have been Fixed from remote
 
-print("220219 a1 a new commit from remote (edit on github)")
+print("220219 a4: a text added here causes a conflict")
 
-print("220219 a2 small text added from local")
+# print("220219 a1 a new commit from remote (edit on github)")
 
-print("220219 a2 small text added from remote")
+# print("220219 a2 small text added from local")
 
-print("220219 a3 small text added from local -- now")
+# print("220219 a2 small text added from remote")
+
+# print("220219 a3 small text added from local -- now")
 
 
